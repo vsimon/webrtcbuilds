ef0905a77480687e8e0bcfefa652b42cf0f19b6c
tnakamura@webrtc.org <tnakamura@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-08-15 14:56:46 +0000

    Merge r6878 into 3.54 branch
    R=andrew@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/15169004
    
    git-svn-id: http://webrtc.googlecode.com/svn/branches/3.54@6910 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/rtp_rtcp/source/rtcp_packet.cc
+++ webrtc/modules/rtp_rtcp/source/rtcp_packet.cc
@@ -429,7 +429,7 @@ void Rpsi::Create(uint8_t* packet, uint16_t* len, uint16_t max_len) const {
 
 void Rpsi::WithPictureId(uint64_t picture_id) {
   const uint32_t kPidBits = 7;
-  const uint64_t k7MsbZeroMask = 0x1ffffffffffffff;
+  const uint64_t k7MsbZeroMask = 0x1ffffffffffffffULL;
   uint8_t required_bytes = 0;
   uint64_t shifted_pid = picture_id;
   do {
