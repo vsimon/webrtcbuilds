eae21d1f3d9f3acc4d8550c3f39ee1a75ef2637a
Vicken Simonian <vsimon@gmail.com>
2015-06-12 12:47:06 -0700

    Don't make thin archives on linux

--- patch.sh
+++ patch.sh
@@ -72,6 +72,8 @@ else
   # enable rtti for osx and linux
   $SED -i "s|'GCC_ENABLE_CPP_RTTI': 'NO'|'GCC_ENABLE_CPP_RTTI': 'YES'|" src/chromium/src/build/common.gypi
   $SED -i "s|^          '-fno-rtti'|          '-frtti'|" src/chromium/src/build/common.gypi
+  # don't make thin archives
+  $SED -i "s|, 'alink_thin'|, 'alink'" src/tools/gyp/pylib/gyp/generator/ninja.py
 fi
 
 popd
