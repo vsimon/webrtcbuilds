f5d01cbc94db92c6205a745affbe7b92cc4eb311
Vicken Simonian <vsimon@gmail.com>
2017-09-20 02:49:07 -0700

    Update repo url and handle upstream moving src/webrtc to src/
    Quote special characters to prevent shell expansion
    Handle the source dirs changing in tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,15 +9,29 @@ WEBRTCBUILDS_FOLDER="$1"
 WEBRTCBUILDS_FOLDER=$(cd $WEBRTCBUILDS_FOLDER && pwd -P)
 CONFIGS=${2:-Debug Release}
 
+# This is for handling breaking changes between WebRTC versions in our test
+WEBRTC_REVISION_NUMBER=$(echo $WEBRTCBUILDS_FOLDER | cut -d- -f2)
+
 tmpdir=$(mktemp -d)
 trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
 pushd $tmpdir >/dev/null
   for CONFIG in $CONFIGS; do
     if [[ $(uname) = Linux ]]; then
       export PKG_CONFIG_PATH=$WEBRTCBUILDS_FOLDER/lib/$CONFIG/pkgconfig
-      c++ -o simple_app $DIR/simple_app.cc $(pkg-config --cflags --libs --define-variable=WEBRTC_LOCAL=$WEBRTCBUILDS_FOLDER libwebrtc_full) -lpthread
+      c++ -o simple_app $DIR/simple_app.cc \
+      -DWEBRTC_REVISION_NUMBER=$WEBRTC_REVISION_NUMBER \
+      $(pkg-config --cflags --libs --define-variable=WEBRTC_LOCAL=$WEBRTCBUILDS_FOLDER libwebrtc_full) \
+      -lpthread
     elif [[ $(uname) = Darwin ]]; then
-      clang++ -o simple_app $DIR/simple_app.cc -DWEBRTC_POSIX -DWEBRTC_MAC -I$WEBRTCBUILDS_FOLDER/include -std=c++11 -stdlib=libc++ $WEBRTCBUILDS_FOLDER/lib/$CONFIG/libwebrtc_full.a -framework Cocoa -framework Foundation -framework IOKit -framework Security -framework SystemConfiguration -framework ApplicationServices -framework CoreServices -framework CoreVideo -framework CoreAudio -framework AudioToolbox -framework QTKit -framework AVFoundation -framework CoreMedia
+      clang++ -o simple_app $DIR/simple_app.cc -DWEBRTC_POSIX -DWEBRTC_MAC \
+        -DWEBRTC_REVISION_NUMBER=$WEBRTC_REVISION_NUMBER \
+        -I$WEBRTCBUILDS_FOLDER/include -std=c++11 -stdlib=libc++ \
+        $WEBRTCBUILDS_FOLDER/lib/$CONFIG/libwebrtc_full.a \
+        -framework Cocoa -framework Foundation -framework IOKit \
+        -framework Security -framework SystemConfiguration \
+        -framework ApplicationServices -framework CoreServices \
+        -framework CoreVideo -framework CoreAudio -framework AudioToolbox \
+        -framework QTKit -framework AVFoundation -framework CoreMedia
     else
       echo No test currently exists for this platform
       exit 1
